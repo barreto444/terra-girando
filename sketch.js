ef0a1af6e1913addbd9412e65eb0e07158cf5ce5
barreto444 <mikaella.barreto.silva@escola.pr.gov.br>
2025-08-12 09:28:47 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
+
 function setup() {
   createCanvas(1920, 1080, WEBGL); //plano 3D
   terra=loadImage('terra.jpg')
 }
 
 function draw() {
-  background(0, 0, 0); //cor de fundo azul
+  background(205, 102, 94); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(200);  //esfera 3D
+  sphere(80, 100); //esfera 3D
   
   
 }
